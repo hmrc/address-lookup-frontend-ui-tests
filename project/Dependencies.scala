e98f30922f88280193129306f143ca98d58493da
ellie-l-newell <113107044+ellie-l-newell@users.noreply.github.com>
2026-02-20 12:11:29 +0000

    Update ui-test-runner dependency to version 0.52.0 for improved testing capabilities

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     "org.scalatestplus"   %% "selenium-3-141"         % "3.2.10.0" % Test,
     "org.slf4j"            % "slf4j-simple"           % "1.7.36"   % Test,
     "com.typesafe.play"   %% "play-ahc-ws-standalone" % "2.1.10"   % Test,
-    "uk.gov.hmrc"         %% "ui-test-runner"         % "0.45.0"   % Test,
+    "uk.gov.hmrc"         %% "ui-test-runner"         % "0.52.0"   % Test,
     "org.tpolecat"        %% "doobie-core"            % "0.13.4"   % Test,
     "org.tpolecat"        %% "doobie-postgres"        % "0.13.4"   % Test,
     "org.tpolecat"        %% "doobie-scalatest"       % "0.13.4"   % Test,
